455c545532e16530f25602b9f304a41048412c6b
monteromario <59968975+monteromario@users.noreply.github.com>
2020-12-19 14:59:27 +0100

    Enemies and lives

--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -9,23 +9,32 @@ class Game {
     this.drawInterval = undefined
 
     this.score = 0
-    this.lives = 0
+    this.lives = 3
+    this.bullets = 0
     this.canRestart = false
 
     this.background = new Background(this.ctx)
     this.macman = new Macman(this.ctx, 720/2, 540/2)
 
-    this.coins = [
-      new Coin(this.ctx, this.randomX(), this.randomY()),
-      
-    ]
+    this.coins = []
+    this.createCoins()
+
+    this.enemies = []
+    this.createEnemies()
+
+    this.mushrooms = []
+    this.flowers = []
   }
     randomX() {
-        return Math.floor(Math.random() * (this.canvas.width - 20));
+        return Math.floor(Math.random() * (this.canvas.width - 40));
     }
 
     randomY() {
-        return Math.floor(Math.random() * (this.canvas.height - 20));
+        return Math.floor(Math.random() * (this.canvas.height - 40));
+    }
+
+    randomV() {
+        return Math.floor(Math.random() * (ENEMIES_MAX_SPEED));
     }
 
   start() {
@@ -47,10 +56,16 @@ class Game {
     this.background.draw()
     this.macman.draw()
     this.coins.forEach(coin => coin.draw())
+    this.enemies.forEach(enemy => enemy.draw())
+    this.mushrooms.forEach(mushroom => mushroom.draw())
+    this.flowers.forEach(flower => flower.draw())
   }
 
   move() {
     this.macman.move()
+    this.enemies.forEach(enemy => enemy.move())
+    this.mushrooms.forEach(mushroom => mushroom.move())
+    this.flowers.forEach(flower => flower.move())
   }
 
   stop() {
@@ -59,14 +74,16 @@ class Game {
       this.ctx.save()
         this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.font = '32px Arial'
-        this.ctx.fillStyle = 'white'
+        this.ctx.font = '64px Press-Start-2P'
+        this.ctx.fillStyle = 'red'
         this.ctx.textAlign = 'center'
         this.ctx.fillText(
         'Game over!',
       this.canvas.width / 2,
       this.canvas.height / 3,
     )
+        this.ctx.font = '32px Press-Start-2P'
+        this.ctx.fillStyle = 'white'
         this.ctx.fillText(
         `Score: ${this.score}`,
       this.canvas.width / 2,
@@ -81,24 +98,57 @@ class Game {
   }
 
   restart() {
-      this.drawInterval = undefined;
-      this.score = 0
-    this.lives = 0
+    this.drawInterval = undefined;
+    this.score = 0
+    this.lives = 3
     this.canRestart = false
 
     this.background = new Background(this.ctx)
     this.macman = new Macman(this.ctx, 720/2, 540/2)
 
-    this.coins = [
-      new Coin(this.ctx, this.randomX(), this.randomY()),
-      
-    ]
-      this.start()
+    this.coins = []
+    this.createCoins()
+    this.enemies = []
+    this.createEnemies()   
+    this.start()
+
+    document.getElementById('icons').innerHTML = ''
+    let i = 0
+    for (i = 0; i < this.lives; i++) {
+        document.getElementById('icons').innerHTML += `<img src="assets/img/mushroom.png" width="25" height="25">`
+    }
   }
 
+  createCoins() {
+    let i = 0
+    for (i = 0; i < INIT_COINS; i++) {
+        this.coins.push(new Coin(this.ctx, this.randomX(), this.randomY()))
+    }
+  }
+
+  createEnemies() {
+    let i = 0
+    for (i = 0; i < INIT_ENEMIES; i++) {
+        this.enemies.push(new Enemy(this.ctx, this.randomX(), this.randomV(), this.randomY(), this.randomV()))
+    }
+  }
+  createMushrooms() {
+    this.mushrooms.push(new Mushroom(this.ctx, this.randomX(), this.randomV(), this.randomY(), this.randomV()))
+  }
+
+  createFlowers() {
+    this.flowers.push(new Flower(this.ctx, this.randomX(), this.randomV(), this.randomY(), this.randomV()))
+  }
 
   updateScore() {
     document.getElementById('score').innerHTML = this.score
+    document.getElementById('lives').innerHTML = this.lives
+    document.getElementById('icons').innerHTML = ''
+    let i = 0
+    for (i = 0; i < this.lives; i++) {
+        document.getElementById('icons').innerHTML += `<img src="assets/img/mushroom.png" width="25" height="25">`
+    }
+    document.getElementById('bullets').innerHTML = this.bullets
   }
 
   onKeyEvent(event) {
@@ -114,15 +164,93 @@ class Game {
     this.macman.onKeyEvent(event)
   }
 
+  damageAnimation() {
+    this.ctx.save()
+    this.ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    clearInterval(this.drawInterval)
+    this.drawInterval = undefined;
+    setTimeout(() => { this.start() }, 600);
+    this.ctx.restore
+  }
+
+  liveAnimation() {
+    this.ctx.save()
+    this.ctx.fillStyle = 'rgba(0, 255, 0, 0.5)'
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.font = '64px Press-Start-2P'
+        this.ctx.fillStyle = 'white'
+        this.ctx.textAlign = 'center'
+        this.ctx.fillText(
+        '1UP!',
+      this.canvas.width / 2,
+      this.canvas.height / 2,
+    )
+    clearInterval(this.drawInterval)
+    this.drawInterval = undefined;
+    setTimeout(() => { this.start() }, 600);
+    this.ctx.restore
+  }
+
+  weaponAnimation() {
+    this.ctx.save()
+    this.ctx.fillStyle = 'rgba(255, 165, 0, 0.5)'
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    clearInterval(this.drawInterval)
+    this.drawInterval = undefined;
+    setTimeout(() => { this.start() }, 300);
+    this.ctx.restore
+  }
+
   checkCollisions() {
-    if (this.coins.length === 0) {
+    
+    if (this.lives === 0) {
         this.stop()
     }
     const restCoins = this.coins.filter(coin => !this.macman.collidesWith(coin))
     const newPoints = this.coins.length - restCoins.length
     this.score += newPoints
     this.updateScore()
+    if (this.coins.length > restCoins.length) {
+        let i = 0
+        for (i = 0; i < INIT_COINS; i++) {
+        restCoins.push(new Coin(this.ctx, this.randomX(), this.randomY()))
+        this.createEnemies()
+        }   
+        if (this.score != 0 && this.score % MUSHROOM_FREQ === 0) {
+        this.createMushrooms()
+        setTimeout(() => { this.mushrooms = [] }, MUSHROOM_TIME);
+        }
+        if (this.score != 0 && this.score % FLOWER_FREQ === 0) {
+        this.createFlowers()
+        setTimeout(() => { this.flowers = [] }, FLOWER_TIME);
+        }
+    }
     this.coins = restCoins
+
+    const restEnemies = this.enemies.filter(enemy => !this.macman.collidesWith(enemy))
+    if (this.enemies.length > restEnemies.length) {
+        this.damageAnimation()
+        this.lives--
+        this.enemies = restEnemies
+        this.updateScore()
+    }
+
+    const restMushrooms = this.mushrooms.filter(mushroom => !this.macman.collidesWith(mushroom))
+    if (this.mushrooms.length > restMushrooms.length) {
+        this.liveAnimation()
+        this.lives++
+        this.mushrooms = restMushrooms
+        this.updateScore()
+    }
+
+    const restFlowers = this.flowers.filter(flower => !this.macman.collidesWith(flower))
+    if (this.flowers.length > restFlowers.length) {
+        this.weaponAnimation()
+        this.bullets++
+        this.flowers = restFlowers
+        this.updateScore()
+    }
 
   }
 }
---
+++ assets/js/models/mushroom.js
@@ -0,0 +1,110 @@
+class Mushroom {
+  constructor(ctx, x, vx, y, vy) {
+    this.ctx = ctx
+
+    this.x = x
+    this.minX = 0
+    this.maxX = this.ctx.canvas.width
+    this.vx = vx
+
+    this.y = y
+    this.minY = 0
+    this.maxY = this.ctx.canvas.height
+    this.vy = vy
+
+    this.width = 0
+    this.height = 0
+
+    this.sprite = new Image()
+    this.sprite.src = './assets/img/mushroom_sprite.png'
+    this.sprite.isReady = false
+    this.sprite.horizontalFrames = 3
+    this.sprite.verticalFrames = 1
+    this.sprite.horizontalFrameIndex = 0
+    this.sprite.verticalFrameIndex = 0
+    this.sprite.drawCount = 0
+    this.sprite.animateCount = 0
+    this.sprite.onload = () => {
+      this.sprite.isReady = true
+      this.sprite.frameWidth = Math.floor(this.sprite.width / this.sprite.horizontalFrames)
+      this.sprite.frameHeight = Math.floor(this.sprite.height / this.sprite.verticalFrames)
+      this.width = this.sprite.frameWidth
+      this.height = this.sprite.frameHeight
+    }
+  }
+
+  isReady() {
+    return this.sprite.isReady
+  }
+
+  clear() {
+
+  }
+
+  draw() {
+    if (this.isReady()) {
+      this.ctx.drawImage(
+        this.sprite,
+        this.sprite.horizontalFrameIndex * this.sprite.frameWidth,
+        this.sprite.verticalFrameIndex * this.sprite.frameHeight,
+        this.sprite.frameWidth,
+        this.sprite.frameHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      )
+
+      this.sprite.drawCount++
+      if (this.sprite.drawCount % ANIMATE_FRAMES === 0) {
+          this.sprite.animateCount++
+      }
+      this.animate()
+    }
+  }
+
+  
+
+  move() {
+
+    this.x += this.vx
+    this.y += this.vy
+
+    if (this.x + this.sprite.frameWidth >= this.maxX) {
+        this.x = this.maxX - this.sprite.frameWidth,
+        this.vx = -this.vx
+        } else if (this.x <= this.minX) {
+        this.x = this.minX;
+        this.vx = -this.vx
+        }
+        
+    if (this.y + this.sprite.frameHeight >= this.maxY) {
+        this.y = this.maxY - this.sprite.frameHeight;
+        this.vy = -this.vy
+        } else if (this.y <= this.minY) {
+        this.y = this.minY
+        this.vy = -this.vy
+        }
+  }
+
+  animate() {
+    if (this.sprite.animateCount % MOVEMENT_FRAMES === 0) {
+        this.sprite.horizontalFrameIndex = 0
+        this.sprite.verticalFrameIndex = 0
+    } else {
+        this.sprite.horizontalFrameIndex = 1
+        this.sprite.verticalFrameIndex = 0
+    } 
+  }
+
+  resetAnimation() {
+    
+  }
+
+  collidesWith(element) {
+    return this.x < element.x + element.width &&
+      this.x + this.width > element.x &&
+      this.y < element.y + element.height &&
+      this.y + this.height > element.y
+  }
+}
